d0f7d162f7a0174502b0896adda80c314bcb0f30
Alison Genin <alison.genin@gmail.com>
2025-03-03 17:04:11 +0100

    add dreamcard style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Deleting all dreams..."
+Dream.destroy_all
+
+puts "Adding new dreams...."
+
+dreams
+
+dreams.each do |dream|
+  Dream.create!(dream)
+  puts "'#{dream[:title]}' has been added!"
+end
+
+puts "All dreams are now in the database!✅"
