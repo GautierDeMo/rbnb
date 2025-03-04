ce7571b433a335888eddbac67f7b6a3edf5b0c43
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-04 15:25:17 +0100

    etape

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
   def dashboard
     @user = current_user
     @bookings = @user.bookings
   end
 
-  #def dashboard
-  #end
 end
