20444836021bfa1e9c5b4cf5254a419207dd98ab
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-03-07 15:41:00 +0100

    infobulle

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,19 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement", "button"]
+  static targets = ["togglableElement", "button"];
 
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
     const mapController = this.application.getControllerForElementAndIdentifier(
       this.togglableElementTarget,
       "map"
-    )
-    if (mapController) {
-      const mapInstance = mapController.getMapInstance()
-      mapInstance.resize()
-    }
+    );
+    if (mapController) mapController.resizeMap();
 
     if (this.togglableElementTarget.classList.contains("d-none")) {
       this.buttonTarget.innerText = "Carte";
